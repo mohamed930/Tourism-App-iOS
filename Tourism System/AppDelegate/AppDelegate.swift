39138e13e8180e948ace4526e4ef6758cb070013
Mohamed Ali <mohammedali12477@gmail.com>
2020-06-07 07:50:48 -0700

    First Commit create project commit

---
+++ Tourism System/AppDelegate/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  Tourism System
+//
+//  Created by Mohamed Ali on 6/7/20.
+//  Copyright © 2020 Mohamed Ali. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
