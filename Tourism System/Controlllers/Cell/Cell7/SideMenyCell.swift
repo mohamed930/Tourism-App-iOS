c07af112bdaea9ba485e492160e54c9714895f4f
Mohamed Ali <mohammedali12477@gmail.com>
2020-08-09 17:19:25 -0700

    Add SideMenu commit
    exit

---
+++ Tourism System/Controlllers/Cell/Cell7/SideMenyCell.swift
@@ -0,0 +1,24 @@
+//
+//  SideMenyCell.swift
+//  Tourism System
+//
+//  Created by Mohamed Ali on 8/8/20.
+//  Copyright © 2020 Mohamed Ali. All rights reserved.
+//
+
+import UIKit
+
+class SideMenyCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ Tourism System/Controlllers/SideMenuViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SideMenuViewController.swift
+//  Tourism System
+//
+//  Created by Mohamed Ali on 8/8/20.
+//  Copyright © 2020 Mohamed Ali. All rights reserved.
+//
+
+import UIKit
+
+class SideMenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
